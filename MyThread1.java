96b0d3d7d1dcddc8564424120a304833bdfbe793
saranyapalani <40590487+saranyapalani@users.noreply.github.com>
2018-09-01 22:03:03 +0530

    Add files via upload

--- MyThread1.java
+++ MyThread1.java
@@ -1,13 +1,12 @@
-package pack;
+package zxc;
 
 public class MyThread1 extends Thread {
-	Table t;  
-	MyThread1(Table t){  
-	this.t=t;  
-	}  
-	public void run(){  
-	t.printTable(0);  
-}  
-	  
-	}  
+		Table t;  
+		MyThread1(Table t){  
+		this.t=t;  
+		}  
+		public void run(){  
+		t.printTable(5);  
+		}   
+		}  
 
--- Table.java
+++ Table.java
@@ -1,14 +1,14 @@
-package pack;
+package zxc;
 
 public class Table {
-	synchronized void printTable(int n){//synchronized method  
-		   for(int i=n;i<=10;i=i+2){  
-		     System.out.println(i);  
-		     try{  
-		      Thread.sleep(400);  
-	     }catch(Exception e){System.out.println(e);}  
-		   }  
-		  
-		 }  
-		}  
+		void printTable(int n){//method not synchronized  
+			   for(int i=1;i<=5;i++){  
+			     System.out.println(n*i);  
+			     try{  
+			      Thread.sleep(400);  
+			     }catch(Exception e){System.out.println(e);}  
+			   }  
+			  
+			 }  
+			}  
 
